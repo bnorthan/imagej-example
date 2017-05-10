0d74c0af5b7b66b33ae8f40cd79184ecefbccde2
Brian Northan <bnorthan@gmail.com>
2017-05-10 06:23:35 -0700

    Add simple imagej example

---
+++ src/main/java/com/example/LaunchImageJ.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import net.imagej.ImageJ;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+public class LaunchImageJ {
+	public static <T extends RealType<T> & NativeType<T>> void main(final String[] args) {
+
+		// create an instance of imagej
+		final ImageJ ij = new ImageJ();
+
+		// launch it
+		ij.launch(args);
+
+		// define an x and y size
+		final int xSize = 128;
+		final int ySize = 128;
+
+		// create a new image
+		final Img<T> img = (Img<T>) ij.op().create().img(new long[] { xSize, ySize });
+
+		// get a 'RandomAccess' object, this object allows access to pixels at
+		// specific locations
+		final RandomAccess<T> ra = img.randomAccess();
+
+		// move to the middle of the image and set the pixel value there to 255
+		ra.setPosition(new long[] { xSize / 2, ySize / 2 });
+		ra.get().setReal(255.0);
+
+		// move the ra to a new position, and set another pixel to 255
+		ra.setPosition(new long[] { xSize / 4, ySize / 4 });
+		ra.get().setReal(255.0);
+
+		// show the image
+		ij.ui().show(img);
+
+	}
+}
