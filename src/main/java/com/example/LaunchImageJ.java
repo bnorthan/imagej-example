2ae24350a289efef1189275b2b9de7833a03a632
Brian Northan <bnorthan@gmail.com>
2017-05-23 21:40:07 -0400

    attempt clearvolume

--- src/main/java/com/example/LaunchImageJ.java
+++ src/main/java/com/example/LaunchImageJ.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import clearvolume.renderer.ClearVolumeRendererInterface;
+import clearvolume.renderer.factory.ClearVolumeRendererFactory;
+import clearvolume.transferf.TransferFunctions;
 import net.imagej.ImageJ;
 import net.imglib2.FinalDimensions;
 import net.imglib2.RandomAccess;
@@ -7,6 +10,7 @@ import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
+
 
 public class LaunchImageJ {
 	public static <T extends RealType<T> & NativeType<T>> void main(final String[] args) {
@@ -16,7 +20,8 @@ public class LaunchImageJ {
 
 		// launch it
 		ij.launch(args);
-
+		
+	
 		// define an x and y size
 		final int xSize = 128;
 		final int ySize = 128;
@@ -40,5 +45,49 @@ public class LaunchImageJ {
 		// show the image
 		ij.ui().show(img);
 
+	}
+	
+	static void clearVolumeExample() {
+		// obtain the best renderer, this usually means picking the best supported GPU programming framwork such as CUDA or OpenCL or GLSL on the most performant GPU installed.
+		final ClearVolumeRendererInterface lClearVolumeRenderer = ClearVolumeRendererFactory.newBestRenderer("ClearVolumeTest",768,768,NativeTypeEnum.UnsignedShort,768,768,2);
+
+		// Different transfer functions can be used:
+		lClearVolumeRenderer.setTransferFunction(TransferFunctions.getGrayLevel());
+		lClearVolumeRenderer.setVisible(true);
+
+		// volume dimensions:
+		final int lResolutionX = 256;
+		final int lResolutionY = 256;
+		final int lResolutionZ = 256;
+
+		// size of the buffer in bytes (16bit data)
+		final int lLengthInBytes = lResolutionX * lResolutionY* lResolutionZ* 2;
+
+		// standard java byte array for holding the data:
+		final byte[] lVolumeDataArray = new byte[lLengthInBytes];
+
+		// Filling the buffer with some dummy yet interesting looking data:
+		for (int z = 0; z < lResolutionZ; z++)
+			for (int y = 0; y < lResolutionY; y++)
+				for (int x = 0; x < lResolutionX; x++)
+				{
+					final int lIndex = 2 * (x + lResolutionX * y + lResolutionX * lResolutionY * z);
+					lVolumeDataArray[lIndex + 1] = (byte) (((byte) x ^ (byte) y ^ (byte) z));
+				}
+
+				
+		// sets the current buffer to be displayed:
+		lClearVolumeRenderer.setVolumeDataBuffer(ByteBuffer.wrap(lVolumeDataArray), lResolutionX, lResolutionY, lResolutionZ);
+
+		// Waits for the renderer's window to be closed:
+		while (lClearVolumeRenderer.isShowing())
+		{
+			Thread.sleep(100);
+		}
+
+		// closes the renderer and release all ressources:
+		lClearVolumeRenderer.close();
+
+		}
 	}
 }
