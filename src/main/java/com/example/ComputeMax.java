da5b3f152554c0f9a1410a7e26260c6c001c0fd3
Brian Northan <bnorthan@gmail.com>
2019-06-07 05:40:00 -0700

    Use map and unary op to implement a ceiling

--- src/main/java/com/example/ComputeMax.java
+++ src/main/java/com/example/ComputeMax.java
@@ -4,10 +4,11 @@ package com.example;
 import java.io.IOException;
 
 import net.imagej.ImageJ;
+import net.imagej.ops.special.computer.AbstractUnaryComputerOp;
+import net.imagej.ops.special.computer.UnaryComputerOp;
 import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.view.Views;
 
 public class ComputeMax {
@@ -27,8 +28,27 @@ public class ComputeMax {
 			"http://imagej.net/images/bridge.gif"); // convenient example stack
 
 		ij.ui().show("bridge", image);
-		
+
 		T max = ij.op().stats().max(image);
+
+		final Img<T> out = ij.op().create().img(image);
+
+		UnaryComputerOp<T, T> op = new AbstractUnaryComputerOp<T, T>() {
+
+			@Override
+			public void compute(T input, T output) {
+				if (input.getRealFloat() > 120) {
+					output.set(input);
+				}
+				else {
+					output.setReal(0);
+				}
+			}
+		};
+
+		ij.op().map(Views.iterable(out), Views.iterable(image), op);
+
+		ij.ui().show(out);
 
 		System.out.println(max.getClass());
 
